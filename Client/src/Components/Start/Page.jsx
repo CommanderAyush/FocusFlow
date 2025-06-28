acf703b77e7b88d2bf0cda8142373f0d791d181b
AYUSH <agrawalayush791@gmail.com>
2025-06-28 13:30:10 +0530

    Linkedin Addition

--- Client/src/Components/Start/Page.jsx
+++ Client/src/Components/Start/Page.jsx
@@ -11,7 +11,7 @@ function Page() {
                 <p className='font-bold text-7xl text-cyan-500'>FOCUSFLOW </p>
                 <p className='mx-2 text-lg'> A new way to boost your productivity</p> 
                 <div className='mx-1 inline bg-cyan-500 text-white px-3 py-2  rounded-4xl text-sm tracking-wider hover:bg-cyan-400'>
-                  <button className='my-5 hover:cursor-pointer'>About Us</button>
+                  <button className='my-5 hover:cursor-pointer'> <a href="https://www.linkedin.com/in/ayush-agrawal-57779a27b/" target="_blank" rel="noopener noreferrer"> About Us</a> </button>
                 </div>   
               </div> 
               
--- Client/src/Pages/EndFooter/EndFooter.jsx
+++ Client/src/Pages/EndFooter/EndFooter.jsx
@@ -7,10 +7,7 @@ function EndFooter() {
     <footer className='flex inset bottom-0 rounder-lg bg-slate-100 px-4 py-1'>
         <p>copyright &copy; {year} FocusFlow |</p>
         <button className='ml-3'>
-            <p>About Us |</p>
-        </button>
-        <button className='ml-3'>
-            <p>About Developers</p>
+            <a href="https://www.linkedin.com/in/ayush-agrawal-57779a27b/" target="_blank" rel="noopener noreferrer"><p>About Developer</p></a>
         </button>
     </footer>
   )
